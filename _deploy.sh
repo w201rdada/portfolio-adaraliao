9b134f3ecc5799c501dac8517f5463e98f7d5909
w201rdada-portfolio-adaraliao-9cdbmobw <adaraliao@berkeley.edu>
2018-03-06 14:14:10 +0000

    deploy

--- _deploy.sh
+++ _deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # put your info here and uncomment so git (and GitHub) knows who your commits belong to!
-# git config user.email "calnet@berkeley.edu"
-# git config user.name "Oski Bear"
+git config --global user.email "adaraliao@berkeley.edu"
+git config --global user.name "Adara Liao"
 
 # make sure you're on the master branch
 git checkout master
